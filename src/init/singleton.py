3c45e57976b5d4cb64bea22362dc6d6290d6af6f
Sudarshan <sudarshan.patil@mactech.net.in>
2026-03-19 16:45:21 +0530

    corrected some changes

--- src/init/singleton.py
+++ src/init/singleton.py
@@ -1,7 +1,10 @@
 import threading
 
+import chromadb
+langchain_google_genai.GoogleGenerativeAI, VertexAIEmbeddings,GoogleGenerativeAIEmbeddings
 from pymongo import MongoClient
 from src.config.configuration import ConfigurationManager
+from src.config.gcp import load_gcp_credentials
 
 
 
@@ -9,8 +12,7 @@ from src.config.configuration import ConfigurationManager
 
 
 
-
-class init:
+class Init:
     _instance = None
     _instance_lock = threading.Lock()
     def __new__(cls):
@@ -18,9 +20,20 @@ class init:
             if cls._instance is None:
                 print('Creating the object')
                 cls._instance = super().__new__(cls)
+                #1.Initialize Config
                 config_obj = ConfigurationManager()
                 config = config_obj.get_base_config()
-                client = MongoClient(config.MONGODB_URI,maxPoolSize=config.MAX_POOL_SIZE)
-                db=client[config.D]
-                user_collection=db[config.collection_user]
-            return
+                cls._instance =config
+                
+                cls._instance.Chroma_client = chromadb.HttpClient(host=config.CHROMA_HOST, port=config.CHROMA_PORT)
+                cls._instance.Client = MongoClient(config.MONGODB_URI,MaxPoolSize = config.MAX_POOL_SIZE)
+                cls._instance.DB =cls._instance.Client[config.DB_NAME]
+                cls._instance.User_Collection = cls._instance.DB[config.collection_user]
+                cls._instance.History_Collection_Name = config.HISTORY_COLLECTION_NAME
+                cls._instance.History_Collection_Logs = config.HISTORY_COLLECTION_Logs
+                cls._instance.Credentials=load_gcp_credentials()
+                cls._instance.Embeddings = GoogleGenerativeAIEmbeddings(model=config.EMBEDD_MODEL, credentials=cls._instance.Credentials)
+                cls._instance.model = GoogleGenerativeAI(model_name = config.RAG_MODEL,temperature=config.temperature,top_p =config.Top_p,top_k=config.Top_k,max_output_tokens=config.Max_output_tokens)
+                
+                # cls._instance.Chatbot_manager = Chatbot_Manager(config=cls._instance.config,credentials=cls._instance.credentials)
+                return cls._instance
